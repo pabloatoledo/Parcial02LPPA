18ebfb0ef85d98d0d756cfec0d492fb4d9977e22
pabloatoledo <103152688+pabloatoledo@users.noreply.github.com>
2022-07-03 18:44:10 -0300

    valida mail y clave contra servidor y modal

--- js/app_dashboard.js
+++ js/app_dashboard.js
@@ -2,11 +2,6 @@ window.onload = function () {
     var linkDatos = "https://basic-server-one.vercel.app/users"
     fetch (linkDatos)
 
-    // .then((response) => response.json()) //2
-    // .then((user) => {
-    //     console.log(user.json()); //3
-    // });
-
     .then(function(respuesta) {
         return respuesta.json()
     })
--- js/app_index.js
+++ js/app_index.js
@@ -1,20 +1,106 @@
 window.onload = function() {
-    fetch("https://basic-server-one.vercel.app/login", 
-        {method: 
-            "POST",
-        headers: 
-            {"Content-Type": "application/json",},
-        body: 
-            JSON.stringify({
-                email: "valeria@gmail.com",
-                password: "lppa2022"})
-    })
-    .then (function(respuesta) {
-        if(respuesta.status === 200) {
-            console.log("valido el usuario")
-        } else {
-            console.log("No valido el usuario")
+    // --------- variables ----------------//
+
+    var camOk
+
+    // --------- busca en html ----------------//
+    
+    var email = document.getElementById("email")
+    var pass = document.getElementById("pass")
+    var errMail = document.getElementById("errMail")
+    var errPass = document.getElementById("errPass")
+    var btnIniSes = document.getElementById("iniSes")
+    var modal = document.getElementById("modalFalla")
+    var span = document.getElementsByClassName("close")[0]
+
+    // --------- inputs ----------------//
+
+    email.addEventListener("blur", valEmail)
+    email.addEventListener("focus", remErrMail)
+    btnIniSes.addEventListener("click", verifTodo)
+
+    span.onclick = function() {
+        modal.classList.remove("block")
+        modal.classList.add("oculto")
         }
-        return respuesta.json()
-    })
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.classList.remove("block")
+            modal.classList.add("oculto")
+        }
+    }
+
+    // --------- funciones validacion campos ----------------//
+
+    function showErrMail () {
+        errMail.classList.remove("oculto")
+    }
+    function remErrMail () {
+        errMail.classList.add("oculto")
+    }
+    function showErrPass () {
+        errPass.classList.remove("oculto")
+    }
+    function remErrPass () {
+        errPass.classList.add("oculto")
+    }
+    function verifTodo () {
+        camOk = 0
+        valEmail()
+        valPass()
+        if (camOk == 2) {
+            validaMail()
+        }
+    }
+
+    // --------- valida campos ----------------//
+
+    function valEmail () {
+        var emailRegex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
+        if (!emailRegex.test(email.value)) {
+            showErrMail()
+        } else {
+            remErrMail()
+            camOk++
+        }
+    }
+    function valPass () {
+        if (pass.value == "") {
+            showErrPass()
+        } else {
+            remErrPass()
+            camOk++
+        }
+    }
+
+    // --------- valida mail y contraseña ----------------//
+    function validaMail () {
+
+        fetch
+        ("https://basic-server-one.vercel.app/login", 
+            {
+            method: 
+                "POST",
+            headers: 
+                {"Content-Type": "application/json",},
+            body: 
+                JSON.stringify({
+                    email: email.value,
+                    password: pass.value})
+            }
+        )
+        .then (function(respuesta) {
+            if(respuesta.status === 200) {
+                alert("Usuario y clave correctos")
+            } else {
+                openModal()
+            }
+            return respuesta.json()
+        })
+    }
+
+    function openModal () {
+        modal.classList.remove("oculto")
+        modal.classList.add("block")
+    }
 }
