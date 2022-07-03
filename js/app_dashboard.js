092b2e4cf2b8579ffb3be73a4ec13a080b0ccacd
pabloatoledo <103152688+pabloatoledo@users.noreply.github.com>
2022-07-03 17:51:01 -0300

    Obtiene datos tabla dashboard por script

--- js/app_dashboard.js
+++ js/app_dashboard.js
@@ -1,3 +1,20 @@
 window.onload = function () {
-    
+    var linkDatos = "https://basic-server-one.vercel.app/users"
+    fetch (linkDatos)
+
+    // .then((response) => response.json()) //2
+    // .then((user) => {
+    //     console.log(user.json()); //3
+    // });
+
+    .then(function(respuesta) {
+        return respuesta.json()
+    })
+    .then(function(datos){
+        //console.log(datos.json())
+        console.log(datos.data[0])
+    })
+    .catch(function(error) {
+        alert("Hubo un problema al enviar los datos, intente nuevamente!!!")
+    })
 }
